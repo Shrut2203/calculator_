1953cc9b17053cd4c638b6ce2bd26ead996f4602
Shruti <151020679+Shrut2203@users.noreply.github.com>
2026-02-22 18:29:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function appear(value) {
+    const display=document. getElementById('display');
+    display.value += value;
+}
+
+function result() {
+    const display=document.getElementById('display');
+    try {
+        const result=display.value.replace(/÷/g,'/').replace(/x/g,'*');
+        display.value=eval(result);
+    }
+    catch {
+        display.value='Error';
+    }
+}
+
+function clearDisplay() {
+    const display=document. getElementById('display');
+    display.value='';
+}
+function lastCharacter() {
+    const display=document. getElementById('display');
+    display.value=display.value.slice(0, -1);
+}
